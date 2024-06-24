655f77b5b81fe0b1ef94528c3461fd980848c893
utibe edet <utibeabasi.okon@myrecharge.ng>
2024-06-24 02:22:07 -0700

    new addition now

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });    
 app.use(
   cors({
-    origin: ["http://localhost:5000", "https://webspinfrontend.vercel.app/"],
+    origin: ["http://localhost:5000", "https://webspinfrontend.vercel.app"],
     credentials: true,
     optionSuccessStatus:200 
   })
